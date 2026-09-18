83b9f1373d753b5ab167d276d58af2cd117e89b6
oct-ayusharma <oct.ayusharma@gmail.com>
2026-09-18 19:16:48 +0530

    Warm Up Syntax

---
+++ warm_up_syntax/02_helloWorld.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+##########################################################
+# Date: 17 Sept 2026
+# Time: 09:13
+# Author: Ayush Sharma
+# Description: Hello World If First Code Of Coding World
+#########################################################
+
+
+# Print Without Flag:
+
+echo -e "Hello, World!"
+
+# Print With Flag:
+
+echo -e "\nHello, Buddy!\nI Am $(whoami)"
+
---
+++ warm_up_syntax/03_comments.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+#########################################################
+# Date: 17 Sept 2026
+# Time: 09:16
+# Author: Ayush Sharma
+# Description: Comments Make Code More Readable
+########################################################
+
+
+# This Is Single-Line Comment
+
+
+<<comment
+...
+This Is
+Multi-Line
+Comment
+...
+comment
+
